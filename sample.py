80684f225b50a8e267204d13821e37235e1f256d
stonegold546 <james.uanhoro@gmail.com>
2020-01-25 03:54:41 -0500

    More params + CORS

--- sample.py
+++ sample.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 from pystan import StanModel
 import falcon
+from falcon_cors import CORS
 import pprint
 from json import dumps, load
 
@@ -17,7 +18,8 @@ def get_percentiles(x):
 
 class TTest:
     def on_post(self, req, resp, **kwargs):
-        raw_data = load(req.bounded_stream)
+        raw_data = load(req.bounded_stream)['params']
+        print(raw_data)
         y1 = pd.Series(raw_data['y1'])
         y0 = pd.Series(raw_data['y0'])
         y_comb = pd.concat([y1, y0])
@@ -32,7 +34,7 @@ class TTest:
             'nu_choice': raw_data['nu_choice']
         }
         params = {new_list: {} for new_list in [
-            'm0', 'm1', 'm_diff', 'st0', 'st1', 'st_ratio']}
+            'm0', 'm1', 'm_diff', 'st0', 'st1', 'st_ratio', 'nu', 'ps']}
         dmdv = pickle.load(open('stan_scripts/dmdv.pkl', 'rb'))
         fit = dmdv.sampling(data=dat_list, chains=4, iter=raw_data['n_iter'])
 
@@ -47,7 +49,7 @@ class TTest:
                 'mean': summary[i][0], 'median': np.median(posteriors[param]),
                 'mcse': summary[i][1], 'sd': summary[i][2],
                 'post': posteriors[param].tolist(), 'ranks': rankdata(posteriors[param]).tolist(),
-                'quantiles': np.quantile(posteriors[param], list(map(lambda x: x / 200, range(1, 200)))).tolist(),
+                # 'quantiles': np.quantile(posteriors[param], list(map(lambda x: x / 200, range(1, 200)))).tolist(),
                 'quantiles': np.quantile(posteriors[param], list(map(lambda x: x / 1000, range(1, 1000)))).tolist(),
                 'ess': summary[i][3], 'rhat': summary[i][4],
                 # todo: 'warnings': 
@@ -59,6 +61,8 @@ class TTest:
         # pp.pprint(params)
         resp.media = params
 
+cors = CORS(allow_all_origins=True, allow_all_methods=True, allow_all_headers=True)
+api = falcon.API(middleware=[cors.middleware])
 
-api = falcon.API()
+# api = falcon.API()
 api.add_route('/two_sample_test', TTest())
